1d010c2792eac757b47c73a0823bf5ddacce469d
DoubleVit <doublevit777@gmail.com>
2024-06-21 14:28:13 +0400

    Added mul function in nums.py Deleted text Added module strings.py

--- first/nums.py
+++ first/nums.py
@@ -6,8 +6,11 @@ def minus(a, b):
     return a - b
 
 
-text = "new text"
+def mul(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(6, 4))
+    print(mul(10, 2))
